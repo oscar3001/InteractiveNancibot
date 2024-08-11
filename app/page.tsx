e7f5176d259d730eff0938774bc7e1607a293c0e
oscar3001 <38573206+oscar3001@users.noreply.github.com>
2024-08-10 21:51:30 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
-'use client'
+"use client";
 
-import InteractiveAvatarCode from '@/components/InteractiveAvatarCode'
+import InteractiveAvatar from "@/components/InteractiveAvatar";
 
 export default function App() {
   return (
     <div className="w-screen h-screen flex flex-col">
-      <div className="flex-grow">
-        <InteractiveAvatarCode />
+      <div className="flex flex-col items-start justify-start w-full h-full">
+        <div className="w-full h-full">
+          <InteractiveAvatar />
+        </div>
       </div>
     </div>
-  )
+  );
 }
