fac28843e54d327af837d9cddbe1344bd59ef99a
oscar3001 <38573206+oscar3001@users.noreply.github.com>
2024-08-11 22:49:36 -0500

    Update InteractiveAvatar.tsx

--- components/InteractiveAvatar.tsx
+++ components/InteractiveAvatar.tsx
@@ -191,7 +191,7 @@ export default function InteractiveAvatar() {
   }
 
   async function startTranscription() {
-    socket.current = new WebSocket("ws://localhost:10001");
+    socket.current = new WebSocket("ws://interactivenancibot.onrender.com");
 
     socket.current.addEventListener("open", () => {
       console.log("WebSocket: connected");
